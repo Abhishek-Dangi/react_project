1dfbf7750796fc13e8d9d0361833ac0a4a923c6b
Abhishek <abhishekdangi2841@gmail.com>
2022-11-10 19:02:19 +0530

    10-11-2022

---
+++ My project on reactjs/server/app.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import * as path from 'path';
+import * as url from 'url';
+import bodyparser from 'body-parser';
+import cors from 'cors';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+var app = express();
+
+//to resolve cross origin problem
+app.use(cors());
+
+// to extract post data
+app.use(bodyparser());
+
+// to get router
+import IndexRouter from './routes/IndexRouter.js';
+import AdminRouter from './routes/AdminRouter.js';
+import UserRouter from './routes/UserRouter.js';
+
+//route level middleware to load specific route as per user
+app.use("/webapiadmin", AdminRouter);
+app.use("/webapiuser", UserRouter);
+app.use("/webapi", IndexRouter);
+
+app.listen(3001);
+console.log("server invoked at link http://localhost:3001"); 
